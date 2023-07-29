822b3cd053500b2fa933efe2f24506881418d743
urielduana <81336230+urielduana@users.noreply.github.com>
2023-07-28 22:22:50 -0600

    Update TagsController.php

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -17,7 +17,7 @@ class TagsController extends Controller
         $notFavoriteTags = Tags::whereDoesntHave('userFavoriteTag', function ($query) use ($auth) {
             $query->where('user_id', $auth->id);
         })->get();
-        return response()->json($notFavoriteTags);
+        return $notFavoriteTags;
     }
 
     /**
