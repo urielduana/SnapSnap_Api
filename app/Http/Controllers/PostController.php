2a9c26fcc894a7b950c2aaa505ad29113b760dc1
Debian <admin@ip-172-31-11-144.us-east-2.compute.internal>
2023-07-29 23:14:20 +0000

    Add functionallty - Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,12 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class PostController extends Controller
+class PostController extends Controller implements HasMedia
 {
+    use InteractsWithMedia;
     /**
      * Display a listing of the resource.
      */
@@ -37,13 +40,63 @@ class PostController extends Controller
 
 public function store(Request $request)
 {
-    $user = Auth::user();
+	try {
+            $caption = $request->input('caption');
+            $tags = explode(',', $request->input('tags'));
+            $images = $request->file('images');
 
-    if (!$user) {
-        return response()->json(['error' => 'Usuario no autenticado'], 401);
-    }
+            // Validar que el usuario esté autenticado
+            $user = Auth::user();
+            if (!$user) {
+                return response()->json(['error' => 'Usuario no autenticado'], 401);
+            }
+            // Validar que se hayan enviado imágenes
+            if (!$images) {
+                return response()->json(['error' => 'No se enviaron imágenes válidas.'], 400);
+            }
 
-    return response()->json(['user' => $user], 200);
+            $urls = [];
+	    $s3 = Storage::disk('s3');
+
+            // Guardar las imágenes en S3
+            foreach ($images as $image) {
+                if (!$image->isValid()) {
+                    continue;
+                }
+
+                $path = 'posts/' . time() . '_' . $image->getClientOriginalName();
+                $s3->put($path, file_get_contents($image), 'public');
+                $url = Storage::url($path);
+                $urls[] = $url;
+	    }
+
+            // Crear el post
+            $post = new Post();
+            $post->user_id = $user->id;
+	    $post->description = $caption;
+	    //Falta arreglar esto de los multiples tags
+            $post->tag_id = $tags[0];
+	    $post->save();
+
+
+	// Guardar las imágenes utilizando Laravel-MediaLibrary
+        foreach ($images as $image) {
+            if (!$image->isValid()) {
+                continue;
+            }
+
+            $media = $post->addMedia($image)
+                          ->toMediaCollection('posts');
+
+            $urls[] = $media->getFullUrl();
+        }
+
+
+
+            return response()->json(['urls' => $urls, 'user' => $user, 'caption' => $caption, 'tags' => $tags], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error al procesar las imágenes.'], 500);
+        }
 }
 
     public function avatar(Request $request){
