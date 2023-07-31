4117e3478a18c4d2a7c0436a65ba8e88e092d19a
urielduana <uri250602@outlook.es>
2023-07-30 23:39:38 -0600

    Update FavoriteTagController.php

--- app/Http/Controllers/FavoriteTagController.php
+++ app/Http/Controllers/FavoriteTagController.php
@@ -67,7 +67,10 @@ class FavoriteTagController extends Controller
     {
         // Receive something like this: {favorite_tags: [{id: 3}, {id: 4}, {id: 6}, {id: 7}, {id: 5}]}
         $favoriteTags = $request->favorite_tags;
-        return $favoriteTags;
+
+        // Get the auth user
+        $auth = auth()->user()->id;
+        return response()->json($auth, $favoriteTags);
     }
 
     /**
