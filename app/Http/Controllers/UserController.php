20833b64b40cf88689f036c7885b7074f92d82a9
urielduana <uri250602@outlook.es>
2023-08-16 11:33:56 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,9 @@ class UserController extends Controller
             ->withCount(['followers as is_following' => function ($query) use ($authUser) {
                 $query->where('follower_id', $authUser->id);
             }])
-            ->withCount('followers')
+            ->with(['followers' => function ($query) {
+                $query->select('id', 'follower_id', 'user_id');
+            }])
             ->get();
 
         foreach ($users as $user) {
